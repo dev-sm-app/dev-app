fb8ad2750f26586b42cd316840637a04af9290b7
Keaton turner <keatonturner@Keatons-MacBook-Pro.local>
2018-10-23 10:13:26 -0600

    sass install

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
-
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const massive = require('massive');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const user = require('./controller/Usercontroller');
+const friend = require('./controller/Friendcontroller');
 
