42bfeaf534f4ee773a9d443523882fdc1d98ec5b
Keaton turner <keatonturner@Keatons-MacBook-Pro.local>
2018-10-23 10:28:07 -0600

    server set up

--- server/server.js
+++ server/server.js
@@ -6,4 +6,34 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const user = require('./controller/Usercontroller');
 const friend = require('./controller/Friendcontroller');
+const mess = require('./controller/Messagecontroller');
+const post =require('./controller/Postcontroller');
+
+const app = express();
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+// User EndPoints //
+
+
+// Friend EndPoints //
+
+
+// Post EndPoints //
+
+
+// Message EndPoints //
+
+
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+
+app.listen(REACT_APP_SERVER_PORT, function(){
+    console.log(`listening on port: ${REACT_APP_SERVER_PORT}`);
+    });
+});
 
