836193369e2891bb41f036ab9db209e5233f0b80
Nathan Blair <nathanburrblair@gmail.com>
2018-10-23 11:28:49 -0600

    login page changes

---
+++ src/components/views/Login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+    render () {
+        return(
+            <div className="mainBody">
+                <header>
+                    <h1>Social Media for Developers</h1>
+                </header>
+                <body>
+                    <button>Log In</button>
+                </body>
+            </div>
+        )
+    }
+}
+
+export default Login;
