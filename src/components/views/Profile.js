e4050a9ed8a79f429bcee84a159340a09d0f8b93
Keaton turner <keatonturner@Keatons-MacBook-Pro.local>
2018-10-24 16:54:36 -0600

    profile page is styled

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -4,18 +4,125 @@ import {userData} from '../../ducks/reducer';
 import {connect} from 'react-redux';
 
 class Profile extends Component {
+  constructor(){
+    super();
+    this.state= {
+      info: true,
+      posts: false,
+      friends: false
+    }
+  }
   componentDidMount(){
     axios.get('/api/auth/setUser').then(res => {
         this.props.userData(res.data);
         console.log(this.props.userData)
     })
 }
+  infoButton(){
+    this.setState({info: true, posts: false, friends: false})
+  }
+  postButton(){
+    this.setState({info: false, posts: true, friends: false})
+  }
+  friendButton(){
+    this.setState({info: false, posts: false, friends: true})
+  }
 
   render() {
-    return (
-      <div>
-        <h1>Profile</h1>
+const info = (this.state.info ? 
+    <div className='info-boxes'>
+      <div className='names'>
+        <div>
+          <p>First Name</p>
+          <input />
+        </div>
+        <div>
+          <p>Last Name</p>
+          <input />
+        </div>
       </div>
+      <div className='types'>
+        <div>
+          <div>
+            <div>
+              <p>Developer Type</p>
+            </div>
+            <div>
+              <select>
+                <option value='Web Development'>Web</option>
+                <option value='IOS Development'>IOS</option>
+                <option value='Salesforce Development'>salesforce</option>
+                <option value='UI/UX Design'>UI/UX</option>
+                <option value='QA Engineer'>QA Engineer</option>
+              </select>
+                
+            </div>
+          </div>
+        <div>
+          <div>
+            <p>Current Employer</p>
+          </div>
+          <div>
+            <input />
+          </div>
+         <div>
+          <button>Save</button>
+         </div>
+        </div> 
+      </div>
+    </div>
+  </div>
+    
+
+   
+    : null);
+const post = (this.state.posts ? 
+    <div><h1>post</h1></div>
+    : null);
+const friend = (this.state.friends ? 
+    <div><h1>friend</h1></div>
+    : null);
+
+
+
+
+    return (
+    <div className='bg-profile'>
+      <div className="img-layer">
+        <div>
+            <img src={this.props.user.picture} alt='' />   
+        </div>
+        <div>
+              <h5>Posts</h5>
+        </div>
+        <div>
+              <h5>Followers</h5>
+        </div>
+        <div>
+            <h5>Following</h5> 
+        </div>
+      </div> 
+      <div className='pers-info'>
+        <div>
+          <h7>Keaton Turner </h7>|<p>Web Developer</p>
+        </div>
+          <p>// this is my bio that could take up alot of charactors so all the people can see it in the screen//</p>
+      </div>
+      <div className='toggle-bar'>
+      <div>
+        <button onClick={() => this.infoButton()}>info</button>
+      </div>
+      <div>
+        <button onClick={() => this.postButton()}>posts</button>
+      </div>
+      <div>
+        <button onClick={() => this.friendButton()}>friends</button>
+      </div>
+      </div>
+      {info}
+      {post}
+      {friend}
+    </div>
     )
   }
 }
