5272fdc491a7ca9e8dd6bef2696b92034db71222
Nathan Blair <nathanburrblair@gmail.com>
2018-10-25 17:01:44 -0600

    Nathan message changes

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,18 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Message extends Component {
-    render() {
-        const className = this.props.user.id === this.props.userid ? 
-        'user_message' : 'friend_message'
-        return(
-            <div className={className}>
-                <div>
-                    <p></p>
-                </div>
-                <img src="" alt=""/>
-            </div>
-        )
-    }
+  render() {
+    const className =
+      this.props.user.id === this.props.userid
+        ? "user_message"
+        : "friend_message";
+    return (
+      <div className={className}>
+        <div>
+          <p />
+        </div>
+        <img src="" alt="" />
+      </div>
+    );
+  }
 }
 
-export default Message;
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps)(Message);
