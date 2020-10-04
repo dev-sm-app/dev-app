30172e4d5fae1859a96d7baf462b6061b07c55ee
Jeremy Coplen <jeremycoplen.jc@gmail.com>
2020-10-04 10:54:56 -0600

    fixed scss pathing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './Styles/main.css';
+import './Styles/main.scss';
 import {Provider} from 'react-redux';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
